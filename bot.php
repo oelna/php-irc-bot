bea0e742a8d7411fc1f1b9b728ed3fe7782a237e
Arno Richter <mail@arnorichter.de>
2020-11-03 15:04:13 +0100

    totally improved new version of the bot script, that can take command line arguments for config files, connect to twitch IRC and includes a pretty decent message parser.

--- bot.php
+++ bot.php
@@ -1,28 +1,5 @@
 <?php
 
-// config
-$server = array(
-    'url' => 'irc.arnorichter.de',
-    'port' => 47362,
-    'ssl' => true
-);
-
-// your user data
-$user = array(
-    'username' => 'oelnabot',
-    'realname' => 'Arno Richter',
-    'nickname' => 'oelnabot',
-    'password' => ''
-);
-
-// which channels to join on login
-$channels = array(
-    '#int', 
-    '#html'
-);
-
-// stop editing here!
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -33,16 +10,23 @@ set_time_limit(0); // Prevent PHP from stopping the script after 30 sec
 DEFINE('DS', DIRECTORY_SEPARATOR);
 DEFINE('EOL', "\n");
 DEFINE('COMMANDS', __DIR__.DS.'commands');
+if(!is_dir(COMMANDS)) { mkdir(COMMANDS); }
 
 require_once(__DIR__.DS.'parser.php');
 
-if(!is_dir(COMMANDS)) {
-    mkdir(COMMANDS);
+// load config
+if(isset($argv[1]) && file_exists($argv[1])) {
+    echo('using custom config file: '.$argv[1].EOL);
+    $config = require_once($argv[1]);
+} else {
+    echo('using default config file: config.php'.EOL);
+    // load the default config
+    $config = require_once(__DIR__.DIRECTORY_SEPARATOR.'config.php');
 }
+$c = $config;
 
 function send($string) {
     global $socket;
-    echo($string.EOL);
     fputs($socket, $string . EOL);
 }
 
@@ -51,40 +35,47 @@ function message($string, $channel) {
 }
 
 // opening the socket to the network
-if(!$socket = fsockopen(($server['ssl'] ? 'ssl://' : '').$server['url'], $server['port'])) {
-    die('Could not establish connection using '.$server['url'].':'.$server['port']);
+if(!$socket = fsockopen(($c['server']['ssl'] ? 'ssl://' : '').$c['server']['url'], $c['server']['port'])) {
+    die('Could not establish connection using '.$c['server']['url'].':'.$c['server']['port']);
 };
 
-// send auth info
-if(!empty($user['password'])) {
-    send('PASS ' . $user['password']);
-}
-send('NICK ' . $user['nickname']);
-send('USER ' . $user['username'] . " 0 * :" . $user['realname']);
+// set timout to 1 second
+if (!stream_set_timeout($socket, 1)) { die('Could not set timeout'); }
 
-send('CAP REQ :message-tags');
-send('CAP REQ :server-time');
-// send('CAP REQ :echo-message');
+// send auth info
+if(!empty($c['user']['password'])) {
+    send('PASS ' . $c['user']['password']);
+}
+send('NICK ' . $c['user']['nickname']);
+send('USER ' . $c['user']['username'] . ' 0 * :' . $c['user']['realname']);
+
+// special capabilities for twitch
+if(substr_compare($c['server']['url'], '.twitch.tv', -strlen('.twitch.tv')) === 0) {
+    send('CAP REQ :twitch.tv/membership');
+    send('CAP REQ :twitch.tv/tags');
+    send('CAP REQ :twitch.tv/commands');
+} else {
+    send('CAP REQ :message-tags');
+    send('CAP REQ :server-time');
+    send('CAP REQ :echo-message');
+}
 send('CAP END');
 
 // force an endless while
 while (1) {
 
-    // continue the rest of the script here
-    while ($data = fgets($socket, 128)) {
-        if(empty($data)) continue;
-        
-        echo $data;
+    // actual message receiving
+    while ($data = fgets($socket, 512)) { // set to 512 to match IRC message length limit
         flush();
-
-        $parsed = Parser::parse($data);
-        // var_dump($parsed);
         
-        // separate all data
-        $ex = explode(' ', $data);
+        echo($data);
 
-        if(!isset($parsed->command)) {
-            // echo('could not parse line: '.implode(',', unpack("C*", $data)).EOL);
+        // separate all data
+        $parsed = Parser::parse($data);
+
+        if($parsed === null || !isset($parsed->command)) {
+            echo('could not parse line: '.$data);
+            echo('characters: '.implode(',', unpack('C*', $data)).EOL);
             continue;
         };
         
@@ -93,21 +84,28 @@ while (1) {
             send(str_replace('PING', 'PONG', $data));
         }
 
-        if(!isset($parsed->command)) continue;
-
         // execute this after MOTD
         if ($parsed->command == 376 || $parsed->command == 422) {
             // join channels
-            foreach($channels as $channel) {
+            foreach($c['channels'] as $channel) {
                 send('JOIN ' . $channel);
             }
         }
 
+        // examples for how to react to messages
+        if(mb_strtoupper($parsed->command) == 'JOIN') {
+            echo((($parsed->prefix->isServer) ? $parsed->prefix->host : $parsed->prefix->nick).' joined channel '.$parsed->params[0].EOL);
+        }
+        
+        if(mb_strtoupper($parsed->command) == 'PART') {
+            echo((($parsed->prefix->isServer) ? $parsed->prefix->host : $parsed->prefix->nick).' left channel '.$parsed->params[0].EOL);
+        }
+
         if(empty($parsed->params)) continue;
 
-        // handle commands
+        // handle !commands
         if ($parsed->command == 'PRIVMSG' && substr($parsed->params[1], 0, 1) == '!') {
-            $nick = $parsed->prefix->nick;
+            $nick = ($parsed->prefix->isServer) ? $parsed->prefix->host : $parsed->prefix->nick;
             $channel = $parsed->params[0];
             $parts = explode(' ', trim($parsed->params[1]));
             $command = ltrim($parts[0], '!');
@@ -117,7 +115,7 @@ while (1) {
             }
 
             // look for custom command function
-            $file = COMMANDS.DIRECTORY_SEPARATOR.trim($command).'.php';
+            $file = COMMANDS.DS.trim($command).'.php';
             if(file_exists($file) && is_file($file)) {
                 $fn = require($file);
                 try {
@@ -138,6 +136,4 @@ while (1) {
     }
 }
 
-// supervisorctl tail php-chatbot
-// supervisorctl <start|stop|restart> php-chatbot
 ?>
